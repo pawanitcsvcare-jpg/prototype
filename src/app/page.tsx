1a7d061ddeb741c34da48a66ad42300d57524e84
Pawan Singh <pawankandari97@gmail.com>
2025-12-16 20:33:16 +0530

    change login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='min-h-screen flex items-center justify-center px-4 w-full login-bgfull'><LoginForm/></div>
+    <div className=''><LoginForm/></div>
   )
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,110 +1,121 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import Image from "next/image"
 import Link from "next/link"
 
-export default function LoginForm({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
+export default function LoginForm() {
   return (
-    <div className={cn("flex flex-col gap-6 w-full max-w-4xl", className)} {...props}>
-      <Card className="overflow-hidden p-0">
-        <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
-            <FieldGroup>
-              <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className="text-muted-foreground text-balance">
-                  Sign in to your account
-                </p>
-              </div>
-              <Field>
-                <FieldLabel htmlFor="email">Username</FieldLabel>
-                <Input
-                  id="username"
-                  type="username"
-                  placeholder="Enter Username"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-2 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </Field>
-              <Field>
-                <Button type="submit">Login</Button>
-              </Field>
-              <Field>
-                <Link href={"/dashboard"}>
-                <Button type="submit" className="w-full">Dashboard</Button>
-                </Link>
-              </Field>
-              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                Or continue with
-              </FieldSeparator>
-              <Field className="grid grid-cols-3 gap-4">
-                <Button variant="outline" type="button">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="sr-only">Login with Apple</span>
-                </Button>
-                <Button variant="outline" type="button">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="sr-only">Login with Google</span>
-                </Button>
-                <Button variant="outline" type="button">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M6.915 4.03c-1.968 0-3.683 1.28-4.871 3.113C.704 9.208 0 11.883 0 14.449c0 .706.07 1.369.21 1.973a6.624 6.624 0 0 0 .265.86 5.297 5.297 0 0 0 .371.761c.696 1.159 1.818 1.927 3.593 1.927 1.497 0 2.633-.671 3.965-2.444.76-1.012 1.144-1.626 2.663-4.32l.756-1.339.186-.325c.061.1.121.196.183.3l2.152 3.595c.724 1.21 1.665 2.556 2.47 3.314 1.046.987 1.992 1.22 3.06 1.22 1.075 0 1.876-.355 2.455-.843a3.743 3.743 0 0 0 .81-.973c.542-.939.861-2.127.861-3.745 0-2.72-.681-5.357-2.084-7.45-1.282-1.912-2.957-2.93-4.716-2.93-1.047 0-2.088.467-3.053 1.308-.652.57-1.257 1.29-1.82 2.05-.69-.875-1.335-1.547-1.958-2.056-1.182-.966-2.315-1.303-3.454-1.303zm10.16 2.053c1.147 0 2.188.758 2.992 1.999 1.132 1.748 1.647 4.195 1.647 6.4 0 1.548-.368 2.9-1.839 2.9-.58 0-1.027-.23-1.664-1.004-.496-.601-1.343-1.878-2.832-4.358l-.617-1.028a44.908 44.908 0 0 0-1.255-1.98c.07-.109.141-.224.211-.327 1.12-1.667 2.118-2.602 3.358-2.602zm-10.201.553c1.265 0 2.058.791 2.675 1.446.307.327.737.871 1.234 1.579l-1.02 1.566c-.757 1.163-1.882 3.017-2.837 4.338-1.191 1.649-1.81 1.817-2.486 1.817-.524 0-1.038-.237-1.383-.794-.263-.426-.464-1.13-.464-2.046 0-2.221.63-4.535 1.66-6.088.454-.687.964-1.226 1.533-1.533a2.264 2.264 0 0 1 1.088-.285z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="sr-only">Login with Meta</span>
-                </Button>
-              </Field>
-              <FieldDescription className="text-center">
-                Don&apos;t have an account? <a href="#">Sign up</a>
-              </FieldDescription>
-            </FieldGroup>
-          </form>
-          <div className="bg-muted relative hidden md:block flex items-center justify-center px-4">
-            <div className="logintext">
-           <h3> Welcome To BlueConnects</h3>
-<p>You are about to access a Vcare corporation system. This computer system and the data therein are property of the Vcare corporation and its clients and provided for official information and use only. Access to this system is restricted to authorized users only. Unauthorized access, use, or modification of this computer system or of the data contained herein, or in transit to/from this system, may constitute a violation of federal or state criminal laws. Anyone who accesses a computer system without authorization or exceeds his or her access authority, or obtains, alters, damages, destroys, or discloses information, or prevents authorized use of information on the computer system, may be subject to administrative penalties, fines or imprisonment.</p>
-</div>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 bg-gray-100">
+
+      {/* LEFT SIDE */}
+      <div
+        className="hidden lg:flex flex-col justify-between bg-cover bg-center p-12"
+        style={{ backgroundImage: "url('/auth-frame.webp')" }}
+      >
+        <div>
+          <h1 className="text-4xl font-bold text-white mb-4">
+            Welcome Back!
+          </h1>
+          <p className="text-white">
+            You are about to access a Vcare corporation system. This computer system and the data therein are property of the Vcare corporation and its clients and provided for official information and use only. Access to this system is restricted to authorized users only. Unauthorized access, use, or modification of this computer system or of the data contained herein, or in transit to/from this system, may constitute a violation of federal or state criminal laws. Anyone who accesses a computer system without authorization or exceeds his or her access authority, or obtains, alters, damages, destroys, or discloses information, or prevents authorized use of information on the computer system, may be subject to administrative penalties, fines or imprisonment.
+          </p>
+        </div>
+
+        <div className="relative mt-10 auth-imgs">
+          <Image
+            src="/img1.PNG"
+            alt="Frame Image"
+            width={420}
+            height={300}
+            className="rounded-lg"
+          />
+
+          <Image
+            src="/img2.PNG"
+            alt="Image"
+            width={150}
+            height={120}
+            className="absolute top-10 right-20 position1 rightmin"
+          />
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className="flex items-center justify-center px-6">
+        <div className="w-full max-w-md">
+
+          {/* Logo */}
+          <div className="text-center mb-6">
+            <Image
+              src="/BlueConnectsLogo.png"
+              alt="Logo"
+              width={190}
+              height={60}
+              className="mx-auto"
+            />
           </div>
-        </CardContent>
-      </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
+
+          {/* Heading */}
+          <div className="text-center mb-8">
+            <h2 className="text-xl font-semibold text-gray-800">
+              Welcome To BlueConnects
+            </h2>
+            <p className="text-gray-500 text-sm">
+              Sign in to access your secure dashboard.
+            </p>
+          </div>
+
+          {/* Form */}
+          <form className="space-y-5">
+
+            {/* Username */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Username
+              </label>
+              <input
+                type="text"
+                placeholder="Username"
+                className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Password */}
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder="********"
+                className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Remember / Forgot */}
+            <div className="flex items-center justify-between text-sm">
+              <label className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="rounded border-gray-300 text-blue-600"
+                />
+                Remember Me
+              </label>
+              <a href="#" className="text-blue-600 hover:underline">
+                Forgot Password?
+              </a>
+            </div>
+
+            {/* Button */}
+             <Link href={"/dashboard"}>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 hover:bg-blue-700 text-white font-medium py-2.5 rounded-lg transition">
+              Login
+            </button>
+            </Link>
+
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
