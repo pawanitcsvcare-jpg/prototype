55c61b20cb247fc63ccd3be44837a48a08f761b2
Pawan Singh <pawankandari97@gmail.com>
2025-12-18 15:46:50 +0530

    error solve

--- src/app/customer-profile/recent-search/page.tsx
+++ src/app/customer-profile/recent-search/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
       style={{
         "--sidebar-width": "calc(var(--spacing) * 72)",
         "--header-height": "calc(var(--spacing) * 12)",
-      }}
+      }as React.CSSProperties}
     >
       <AppSidebar variant="inset" />
       <SidebarInset>
