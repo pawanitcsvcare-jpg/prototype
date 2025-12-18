f9bb3af714c26b4e6d360414f4a2c5cbc434e8f8
Pawan Singh <pawankandari97@gmail.com>
2025-12-18 15:50:36 +0530

    reeor solve

--- src/app/customer/aysnc-response/page.tsx
+++ src/app/customer/aysnc-response/page.tsx
@@ -124,7 +124,7 @@ export default function AysncResponse() {
         {
           "--sidebar-width": "calc(var(--spacing) * 72)",
           "--header-height": "calc(var(--spacing) * 12)",
-        } 
+        }as React.CSSProperties
       }
     >
       <AppSidebar variant="inset" />
