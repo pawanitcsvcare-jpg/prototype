84571ac70a686e165adb46d9892f235ae62fc029
Pawan Singh <pawankandari97@gmail.com>
2025-12-20 13:53:31 +0530

    fix error

--- src/lib/recent-search-data.ts
+++ src/lib/recent-search-data.ts
@@ -1,20 +1,28 @@
-export const recentSearchDataColumns = [
-  { key: "srno", label: "Sr No" },
-  { key: "customerid", label: "Customer ID" },
-  { key: "msisdn", label: "MSISDN" },
-  { key: "sim", label: "SIM" },
-  { key: "searchedBy", label: "Searched By" },
+export type RecentSearchRow = {
+  srno: number
+  customerid: string
+  msisdn: string
+  sim: string
+  searchedBy: string
+}
+
+export const recentSearchDataColumns: {
+  label: string
+  key: keyof RecentSearchRow
+}[] = [
+  { label: "Sr No", key: "srno" },
+  { label: "Customer ID", key: "customerid" },
+  { label: "MSISDN", key: "msisdn" },
+  { label: "SIM", key: "sim" },
+  { label: "Searched By", key: "searchedBy" },
 ]
 
-export const recentSearchData = [
-  { srno: 1, customerid: "10546", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 2, customerid: "10565", msisdn: "561-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 3, customerid: "10546", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 4, customerid: "87654", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 5, customerid: "10540", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 6, customerid: "10541", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 7, customerid: "10542", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 8, customerid: "47679", msisdn: "332-280-4351", sim: "89012802331621206999", searchedBy: "Supadmin" },
-  { srno: 9, customerid: "54575", msisdn: "332-280-1443", sim: "89012802331621200034", searchedBy: "Supadmin" },
-  { srno: 10, customerid: "10542", msisdn: "332-280-1461", sim: "89012802331621200034", searchedBy: "Supadmin" },
+export const recentSearchData: RecentSearchRow[] = [
+  {
+    srno: 1,
+    customerid: "10546",
+    msisdn: "332-280-1461",
+    sim: "89012802331621200034",
+    searchedBy: "System",
+  },
 ]
